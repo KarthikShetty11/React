ad6324468e0238057a8abefbb563d54e18e84d57
KarthikShetty11 <kvshetty27@gmail.com>
2022-04-02 14:32:14 +0530

    component added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Content from './Content'
 
-ReactDOM.render(<h1>Hello world</h1>,document.getElementById("root"));
+ReactDOM.render(root ,document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
